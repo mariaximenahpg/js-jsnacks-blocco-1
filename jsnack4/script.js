0934fd09cc2e0bd1eabfcc545926f4d99d189a5c
mariaximenahpg <federico.businessrico@gmail.com>
2022-10-28 11:03:43 +0200

    modifica if

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -5,7 +5,7 @@ const numbers=[2,3,1,9,6,3,7,9,2,4,1];
 let oddnumbers=0;
 for(let i=0; i<numbers.length; i++){
     console.log(numbers[i]);
-    if(i!=0 && i%2===0) {
+    if(i%2===0) {
     sum += numbers[i];
     }
 }
